f27cd822291d7a42c653c1a0dc4373a902bedaa8
ELEF-TQ <tariq75aitlahklift@gmail.com>
2024-02-21 19:57:19 +0100

    setup server

---
+++ Server/src/test/java/com/ParcAuto/Ensa/EnsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ParcAuto.Ensa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
