367b2e058cc780759047ce5fe08398db03096fb2
ELEF-TQ <tariq75aitlakhlift@gmail.com>
2024-03-20 22:21:38 +0000

    driver affectation to TRIP by permis

--- Server/src/main/java/com/ParcAuto/Ensa/Affectation/Dto/TripDTO.java
+++ Server/src/main/java/com/ParcAuto/Ensa/Affectation/Dto/TripDTO.java
@@ -21,8 +21,7 @@ public class TripDTO {
     private Time arrivalTime;
     private int nbrOfPassengers;
     private String statusConfirmation;
-
-
     private VehiculeType vehiculType;
+    private DriverDTO driver;
 
 }
