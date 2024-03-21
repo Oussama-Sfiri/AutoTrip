5d590154931161bfa521b85d5e322e904461ac57
unknown <sfirioussama@gmail.com>
2024-03-21 10:36:23 +0000

    Affectation controller/service

---
+++ Server/src/main/java/com/ParcAuto/Ensa/Affectation/Controllers/AffectationController.java
@@ -0,0 +1,9 @@
+package com.ParcAuto.Ensa.Affectation.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/affectation")
+public class AffectationController {
+}
