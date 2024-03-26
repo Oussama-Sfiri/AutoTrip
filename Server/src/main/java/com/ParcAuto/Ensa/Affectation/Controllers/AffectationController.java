60a99c77835753df322a89394f0615f6dd86a37e
unknown <sfirioussama@gmail.com>
2024-03-26 23:01:38 +0000

    affectation not yet completed

--- Server/src/main/java/com/ParcAuto/Ensa/Affectation/Controllers/AffectationController.java
+++ Server/src/main/java/com/ParcAuto/Ensa/Affectation/Controllers/AffectationController.java
@@ -1,9 +1,36 @@
 package com.ParcAuto.Ensa.Affectation.Controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ParcAuto.Ensa.Affectation.Dto.DriverDTO;
+import com.ParcAuto.Ensa.Affectation.Dto.VehiculeDTO;
+import com.ParcAuto.Ensa.Affectation.Services.AffectationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/affectation")
 public class AffectationController {
+    @Autowired
+    private AffectationService affectationService;
+
+    @GetMapping("/conducteurs-disponibles/{tripId}")
+    public ResponseEntity<List<DriverDTO>> getConducteursDisponibles(@PathVariable Long tripId) {
+        List<DriverDTO> conducteursDisponibles = affectationService.getConducteursDisponibles(tripId);
+        return ResponseEntity.ok(conducteursDisponibles);
+    }
+
+    @GetMapping("/vehicules-disponibles/{tripId}")
+    public ResponseEntity<List<VehiculeDTO>> getVehiculesDisponibles(@PathVariable Long tripId) {
+        List<VehiculeDTO> vehiculesDisponibles = affectationService.getVehiculesDisponibles(tripId);
+        return ResponseEntity.ok(vehiculesDisponibles);
+    }
+
+    @PostMapping("/affecter-trip/{tripId}")
+    public ResponseEntity<String> affecterTrip(@RequestParam Long conducteurId, @RequestParam Long vehiculeId, @PathVariable Long tripId) {
+        String message = affectationService.affecterTrip(conducteurId, vehiculeId, tripId);
+        return ResponseEntity.ok(message);
+    }
 }
