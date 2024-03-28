b978385c93168106b90d7d41bb04f30145b6528a
ELEF-TQ <tariq75aitlakhlift@gmail.com>
2024-03-28 10:40:49 +0000

    merge

--- Server/src/main/java/com/ParcAuto/Ensa/Affectation/Repositories/VehiculeRepository.java
+++ Server/src/main/java/com/ParcAuto/Ensa/Affectation/Repositories/VehiculeRepository.java
@@ -15,14 +15,5 @@ import java.util.Optional;
 public interface VehiculeRepository extends JpaRepository<Vehicule, Long> {
 
 
-    @Query("SELECT v FROM Vehicule v WHERE v.disponibilite = true AND v.typePermisRequis = :permitType " +
-            "AND NOT EXISTS (SELECT t FROM Trip t WHERE t.vehicule = v AND " +
-            "(:departureDate BETWEEN t.departureDate AND t.arrivalDate " +
-            "OR :arrivalDate BETWEEN t.departureDate AND t.arrivalDate " +
-            "OR t.departureDate BETWEEN :departureDate AND :arrivalDate " +
-            "OR t.arrivalDate BETWEEN :departureDate AND :arrivalDate))")
-    List<Vehicule> getAvailableVehiclesForTrip(@Param("permitType") PermisType permitType,
-                                               @Param("departureDate") Date departureDate,
-                                               @Param("arrivalDate") Date arrivalDate);
 
 }
