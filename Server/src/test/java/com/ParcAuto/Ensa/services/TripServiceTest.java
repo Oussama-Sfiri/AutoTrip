68a6db4bf775c044cd809cb7d69c7654cf6917a7
ELEF-TQ <tariq75aitlahklift@gmail.com>
2024-04-03 23:21:19 +0000

    more creation tests

--- Server/src/test/java/com/ParcAuto/Ensa/services/TripServiceTest.java
+++ Server/src/test/java/com/ParcAuto/Ensa/services/TripServiceTest.java
@@ -58,6 +58,69 @@ public class TripServiceTest {
         assertNotNull(responseEntity.getBody());
     }
 
+    @Test
+    public void testDepartureDateBeforeCurrentDate() {
+        // Mocking tripDTO with departure date before the current date
+        TripDTO tripDTO = new TripDTO();
+        tripDTO.setDeparture("Agadir");
+        tripDTO.setDestination("Madrid");
+        tripDTO.setDepartureDate(LocalDate.now().minusDays(1)); // Setting departure date to yesterday
+        tripDTO.setDepartureTime(Time.valueOf(LocalTime.of(9, 0)));
+        tripDTO.setArrivalDate(tripDTO.getDepartureDate().plusDays(1));
+        tripDTO.setArrivalTime(Time.valueOf(LocalTime.of(8, 0)));
+        tripDTO.setNbrOfPassengers(5);
+        tripDTO.setVehiculType(VehiculeType.Fourgonette);
+
+        // Performing the test
+        ResponseEntity<?> responseEntity = tripService.createTrip(tripDTO);
+
+        // Assertions
+        assertNotNull(responseEntity);
+        assertEquals(400, responseEntity.getStatusCode().value());
+    }
+
+    @Test
+    public void testArrivalDateBeforeDepartureDate() {
+        // Mocking tripDTO with arrival date before departure date
+        TripDTO tripDTO = new TripDTO();
+        tripDTO.setDeparture("Agadir");
+        tripDTO.setDestination("Madrid");
+        tripDTO.setDepartureDate(LocalDate.now().plusDays(1));
+        tripDTO.setDepartureTime(Time.valueOf(LocalTime.of(9, 0)));
+        tripDTO.setArrivalDate(tripDTO.getDepartureDate().minusDays(1)); // Setting arrival date before departure date
+        tripDTO.setArrivalTime(Time.valueOf(LocalTime.of(8, 0)));
+        tripDTO.setNbrOfPassengers(5);
+        tripDTO.setVehiculType(VehiculeType.Fourgonette);
+
+        // Performing the test
+        ResponseEntity<?> responseEntity = tripService.createTrip(tripDTO);
+
+        // Assertions
+        assertNotNull(responseEntity);
+        assertEquals(400, responseEntity.getStatusCode().value());
+    }
+
+    @Test
+    public void testArrivalTimeBeforeDepartureTime() {
+        // Mocking tripDTO with arrival time before departure time on the same day
+        TripDTO tripDTO = new TripDTO();
+        tripDTO.setDeparture("Agadir");
+        tripDTO.setDestination("Madrid");
+        tripDTO.setDepartureDate(LocalDate.now()); // Setting departure date to the current date
+        tripDTO.setDepartureTime(Time.valueOf(LocalTime.of(9, 0)));
+        tripDTO.setArrivalDate(tripDTO.getDepartureDate());
+        tripDTO.setArrivalTime(Time.valueOf(LocalTime.of(8, 0))); // Setting arrival time before departure time
+        tripDTO.setNbrOfPassengers(5);
+        tripDTO.setVehiculType(VehiculeType.Fourgonette);
+
+        // Performing the test
+        ResponseEntity<?> responseEntity = tripService.createTrip(tripDTO);
+
+        // Assertions
+        assertNotNull(responseEntity);
+        assertEquals(400, responseEntity.getStatusCode().value());
+    }
+
 
 
 
